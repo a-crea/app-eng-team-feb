21c68f6f9b76d5f39eb96d9c22fc69a57b140b6a
a-crea <58514653+a-crea@users.noreply.github.com>
2020-04-24 11:16:22 +0200

    Add patient draft

---
+++ php/add-patient.php
@@ -0,0 +1,19 @@
+<?php
+    $name = $_POST["name"];
+    $surname = $_POST["surname"];
+    $birthDate = $_POST["birthDate"];
+    $gender = $_POST["gender"];
+    $email = $_POST["email"];
+
+    print_r($name);
+    echo"<br>";
+    print_r($surname);
+    echo"<br>";
+    print_r($birthDate);
+    echo"<br>";
+    print_r($gender);
+    echo"<br>";
+    print_r($email);
+
+    //TO INSERT IN DB
+?>

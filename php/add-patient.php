0e533ccdfd8e98801ca0d3f680dacf2e9071b58e
a-crea <58514653+a-crea@users.noreply.github.com>
2020-04-30 16:09:44 +0200

    DB Schema draft

---
+++ php/add-patient.php
@@ -0,0 +1,15 @@
+<?php
+    $name = $_POST["name"];
+    $surname = $_POST["surname"];
+    $birthDate = $_POST["birthDate"];
+    $gender = $_POST["gender"];
+    $email = $_POST["email"];
+
+    print_r($name);
+    print_r($surname);
+    print_r($birthDate);
+    print_r($gender);
+    print_r($email);
+
+    //TO INSERT IN DB
+?>

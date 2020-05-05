74522b783768c32a66f90e9a10a114edd377f482
a-crea <58514653+a-crea@users.noreply.github.com>
2020-05-05 14:33:22 +0200

    added template file

---
+++ documentation/template.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php
+      include (dirname(__FILE__).'/components/head.php');
+  ?>
+  <title>TITLE</title>
+</head> 
+<body>
+    <?php
+        $active='patients|appointmens|index';
+        include (dirname(__FILE__).'/components/navbar.php');
+    ?>
+    <div class="container mt-4">
+      CODE HERE
+    </div>
+    <?php
+        include (dirname(__FILE__).'/components/footer.php');
+    ?>
+  </body>
+</html>

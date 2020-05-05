30941d47c146099b12c1803c288d4a3149937e3d
a-crea <58514653+a-crea@users.noreply.github.com>
2020-05-05 13:53:03 +0200

    from html to php

---
+++ src/appointment-view.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        include (dirname(__FILE__).'/components/head.php');
+    ?>
+    <title>Appointments</title>
+</head>
+
+<body>
+    <?php
+        include (dirname(__FILE__).'/components/navabar.php');
+    ?>
+    <div class="container mt-4">
+        <form action="">
+            <div class="form-group">
+                <label for="date">
+                    Date:
+                </label>
+                <input type="date" name="date" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="time">
+                    Time:
+                </label>
+                <input type="text" name="time" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="patient">
+                    Patient:
+                </label>
+                <select name="patient" class="selectpicker form-control" data-live-search="true">
+                    <option>Mustard</option>
+                    <option>Ketchup</option>
+                    <option>Barbecue</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="treatment">
+                    Treatment:
+                </label>
+                <select name="treatment" class="selectpicker form-control" data-live-search="true">
+                    <option>Mustard</option>
+                    <option>Ketchup</option>
+                    <option>Barbecue</option>
+                </select>
+            </div>
+            <div class="form-row py-2">
+                <div class="col-6 col-sm-6 col-md-3 col-lg-2 mr-md-2">
+                    <button type="submit" class="btn btn-success btn-block">Save</button>
+                </div>
+                <div class="col-6 col-sm-6 col-md-3 col-lg-2 ml-md-2">
+                    <button type="reset" class="btn btn-danger btn-block">Delete</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <?php
+        include (dirname(__FILE__).'/components/footer.php');
+    ?>
+</body>
+
+</html>
---
+++ src/components/navbar.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="index.html">Dentist Management System</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item" id="nav-index-link">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item" id="nav-index-link">
+                <a class="nav-link" href="appointments.php">Appointments</a>
+            </li>
+            <li class="nav-item" id="nav-index-link">
+                <a class="nav-link" href="patients.php">Patients</a>
+            </li>
+        </ul>
+    </div>
+</nav>
